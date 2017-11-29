7ee792b9d9dc45c7030ce32d8ce4437d4870c7f5
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2017-11-29 20:43:00 +0900

    commit

--- OpenGL/OpenGL/src/VertexArray.cpp
+++ OpenGL/OpenGL/src/VertexArray.cpp
@@ -114,6 +114,11 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	// create window and context with the core profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // want my major version to be OpenGL 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // minor version to be 3 (3.3)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // setting OpenGL profile to be core (was compatibility profile)
+
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
@@ -122,9 +127,9 @@ int main(void)
 	}
 
 	glfwMakeContextCurrent(window);
-
+	
 	// limiting our frame rates
-	//glfwSwapInterval(1); 
+	glfwSwapInterval(1); 
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error!" << std::endl;
@@ -133,17 +138,20 @@ int main(void)
 
 	float positions[] = {
 		-0.5f, -0.5f, // 0
-		0.5f, -0.5f, // 1
-		0.5f,  0.5f, // 2
+		 0.5f, -0.5f, // 1
+		 0.5f,  0.5f, // 2			 
+		-0.5f,  0.5f, // 3
 
-	    // 0.5f,  0.5f,
-	   -0.5f,  0.5f, // 3
-		// -0.5f, -0.5f
 	};
 	unsigned int indices[] = { // index buffer 
 		0, 1, 2,
 		2, 3, 0
 	};
+
+	// create VAO
+	unsigned int vao; // hold vertex object array id
+	GLCall(glGenVertexArrays(1, &vao)); // generate 1 of them and store id in vao
+	GLCall(glBindVertexArray(vao)); // there is no actual target (like GL_ARRAY_BUFFER) so we just specify the id
 
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
@@ -152,6 +160,7 @@ int main(void)
 
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+	// this line of code links this buffer to the VAO 
 
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
@@ -159,24 +168,43 @@ int main(void)
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-
+	
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
 
 	// retrieve the location of the variable
-	GLCall(int location = glGetUniformLocation(shader, "u_Color")); 
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
 	// make sure spell in exact same case as you do with actual variable name in your shader
 	ASSERT(location != -1); // if location is -1, it means could not find uniform
 	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+	// unbind everything
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	float r = 0.0f;
 	float increment = 0.05f;
 	while (!glfwWindowShouldClose(window))
 	{
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+		// before drawing bind 
+		GLCall(glUseProgram(shader));
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f)); // set the uniform once shader bound 
 		
+		// no longer have to do enable or pointer code and don't need to bind buffer
+		// GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+		// GLCall(glEnableVertexAttribArray(0));
+		// GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
+		// not binding vertex buffer at all and currently bound buffer to glarraybuffer is 0
+		// just binding vertexarray and inde buffer
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		// draw call
-		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f) increment = -0.05f;
